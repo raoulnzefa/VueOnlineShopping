0d0d75139453bc7651f67b4681275b1c430014f0
LeoGao <973525427@qq.com>
2022-03-29 15:41:57 +0800

    "Second Push": Set template

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,14 +1,21 @@
 <template>
-  
+  <ul class="todo-main">
+    <MyItem
+      v-for="todoObj in todos"
+      :key="todoObj.id"
+      :todo="todoObj"
+      :checkTodo="checkTodo"
+      :deleteTodo="deleteTodo"
+    />
+  </ul>
 </template>
 
 <script>
-import ToDoListItem from './components/ToDoListItem.vue'
+import ToDoListItem from "./components/ToDoListItem.vue";
 export default {
-    name:"ToDoList",
-}
+  name: "ToDoList",
+};
 </script>
 
 <style>
-
 </style>
--- src/components/ToDoListHeader.vue
+++ src/components/ToDoListHeader.vue
@@ -1,14 +1,19 @@
 <template>
-  
+  <div class="todo-header">
+    <input
+      type="text"
+      placeholder="请输入你的任务名称，按回车键确认"
+      v-model="title"
+      @keyup.enter="add"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    name:"ToDoListHeader",
-    
-}
+  name: "ToDoListHeader",
+};
 </script>
 
 <style>
-
 </style>
